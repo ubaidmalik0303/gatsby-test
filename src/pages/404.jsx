cd5179b1b248a5edda15733d4444c0c806121409
ubaidmalik0303 <ubaidmalik0303@gmail.com>
2022-03-13 19:24:08 +0500

    add 404 page

---
+++ src/pages/404.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../Components/Layout"
+import { Link } from "gatsby"
+
+export default function About() {
+  return (
+    <>
+      <Layout>
+        <h1>Sorry Page Not Found!</h1>
+        <Link>Go To Home Page</Link>
+      </Layout>
+    </>
+  )
+}
